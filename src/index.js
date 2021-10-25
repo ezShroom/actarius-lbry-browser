cc7f1d55ab3e5bd48c7b3cd71b37d58812204137
Shroom2020 <hello-shroom@protonmail.com>
2021-10-25 15:30:29 +0100

    Do something, I don't actually remember what it was now

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 const createWindow = () => {
   	// Create the browser window.
   	const mainWindow = new BrowserWindow({
-    	// width: 800,
-    	// height: 600,
+    	width: 800,
+    	height: 600,
     	webPreferences: {
       		webviewTag: true
     	}
